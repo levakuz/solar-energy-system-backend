33fc44e42ae971261b1c819ca525415b738a6d75
levakuz <levakuz007@gmail.com>
2023-06-01 23:49:07 +0200

    added: CORS middleware

--- src/entrypoints/app.py
+++ src/entrypoints/app.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from src.auth.router import router as auth_router
 from src.accounts.router import account_router
 from src.database import init_database
 
 app = FastAPI()
 
-app.include_router(auth_router)
-app.include_router(account_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router, prefix='/api/v1')
+app.include_router(account_router, prefix='/api/v1')
 
 app.add_event_handler('startup', init_database)
