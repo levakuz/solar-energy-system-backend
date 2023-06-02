5b82929860cee658f5cd03343da9c2b6a3a71a68
levakuz <levakuz007@gmail.com>
2023-06-02 18:43:12 +0200

    added: inactive user global exception handling

--- src/entrypoints/app.py
+++ src/entrypoints/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
 
-from src.auth.router import router as auth_router
+from src.accounts.exceptions import InactiveUserException
 from src.accounts.router import account_router
+from src.auth.router import router as auth_router
 from src.database import init_database
 
 app = FastAPI()
@@ -15,8 +17,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 app.include_router(auth_router, prefix='/api/v1')
 app.include_router(account_router, prefix='/api/v1')
 
 app.add_event_handler('startup', init_database)
+
+
+@app.exception_handler(InactiveUserException)
+async def validation_exception_handler(request, err):
+    return JSONResponse(status_code=400, content={"detail": f"{err}"})
