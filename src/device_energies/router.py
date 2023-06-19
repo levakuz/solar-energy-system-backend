1f3fa19f83f4fbafe5330163fdf341d133da7641
levakuz <levakuz007@gmail.com>
2023-06-19 15:16:24 +0200

    added: device energy exception handling in router & uow

--- src/device_energies/router.py
+++ src/device_energies/router.py
@@ -2,9 +2,11 @@ from typing import Annotated
 
 import fastapi
 from fastapi import Depends
+from starlette.responses import JSONResponse
 
 from src.core.unit_of_work import AbstractUnitOfWork
 from src.device_energies.domain import DeviceEnergy
+from src.device_energies.exceptions import DeviceEnergyDoesNotExistsException
 from src.device_energies.schemas import DeviceEnergyCreateSchema
 from src.device_energies.unit_of_work import DeviceEnergyUnitOfWork
 
@@ -32,7 +34,10 @@ async def get_device_energy(
             Depends(DeviceEnergyUnitOfWork)
         ],
 ):
-    return await device_energy_uow.get(id=id)
+    try:
+        return await device_energy_uow.get(id=id)
+    except DeviceEnergyDoesNotExistsException as e:
+        return JSONResponse(status_code=404, content={'detail': e.message})
 
 
 @device_energy_router.put("/{id}", response_model=DeviceEnergy, tags=['Device Energies'])
@@ -44,7 +49,10 @@ async def update_device_energy(
             Depends(DeviceEnergyUnitOfWork)
         ],
 ):
-    return await device_energy_uow.update(id=id, update_object=form_data)
+    try:
+        return await device_energy_uow.update(id=id, update_object=form_data)
+    except DeviceEnergyDoesNotExistsException as e:
+        return JSONResponse(status_code=404, content={'detail': e.message})
 
 
 @device_energy_router.delete("/{id}", tags=['Device Energies'])
@@ -55,4 +63,7 @@ async def delete_device_energy(
             Depends(DeviceEnergyUnitOfWork)
         ],
 ):
-    return await device_energy_uow.delete(id=id)
+    try:
+        return await device_energy_uow.delete(id=id)
+    except DeviceEnergyDoesNotExistsException as e:
+        return JSONResponse(status_code=404, content={'detail': e.message})
