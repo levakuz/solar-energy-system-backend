0e75cdf27fc76864cb0f834593a6f1cce630a379
levakuz <levakuz007@gmail.com>
2023-06-19 15:25:44 +0200

    fixed: device types exception handling in router & uow

--- src/device_types/router.py
+++ src/device_types/router.py
@@ -2,9 +2,11 @@ from typing import Annotated
 
 import fastapi
 from fastapi import Depends
+from starlette.responses import JSONResponse
 
 from src.core.unit_of_work import AbstractUnitOfWork
 from src.device_types.domain import DeviceType
+from src.device_types.exceptions import DeviceTypeDoesNotExistsException
 from src.device_types.schemas import DeviceTypeCreateSchema
 from src.device_types.unit_of_work import DeviceTypeUnitOfWork
 
@@ -32,7 +34,10 @@ async def get_device_type(
             Depends(DeviceTypeUnitOfWork)
         ],
 ):
-    return await company_account_uow.get(id=id)
+    try:
+        return await company_account_uow.get(id=id)
+    except DeviceTypeDoesNotExistsException as e:
+        return JSONResponse(status_code=404, content={'detail': e.message})
 
 
 @device_type_router.put("/{id}", response_model=DeviceType, tags=['Device Types'])
@@ -44,7 +49,10 @@ async def update_device_type(
             Depends(DeviceTypeUnitOfWork)
         ],
 ):
-    return await company_account_uow.update(id=id, update_object=form_data)
+    try:
+        return await company_account_uow.update(id=id, update_object=form_data)
+    except DeviceTypeDoesNotExistsException as e:
+        return JSONResponse(status_code=404, content={'detail': e.message})
 
 
 @device_type_router.delete("/{id}", tags=['Device Types'])
@@ -55,4 +63,7 @@ async def delete_device_type(
             Depends(DeviceTypeUnitOfWork)
         ],
 ):
-    return await company_account_uow.delete(id=id)
+    try:
+        return await company_account_uow.delete(id=id)
+    except DeviceTypeDoesNotExistsException as e:
+        return
